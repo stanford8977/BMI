8778deb6f4c5e0a3f231c5b9c91b00e777bcb0f8
stanford8977 <stanford8977@gmail.com>
2019-10-15 14:39:41 +0800

    0220210

---
+++ app/src/main/java/com/stanford/MainActivity.java
@@ -0,0 +1,19 @@
+package com.stanford;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  static final String  TAG  = MainActivity.class.getSimpleName();
+    private EditText edWeight;
+    private EditText edHeight;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+
+    }
+}
